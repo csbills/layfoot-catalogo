422ccdf26f5f589e7c8768d52343f98e10cf38a3
csbills <caio.henrique1000@hotmail.com>
2021-08-09 11:51:57 -0300

    commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,8 +2,18 @@ import commerce from '../lib/commerce';
 import Image from 'next/image';
 
 import logo from '../public/layfoot_preto.png';
+import { useEffect, useState } from 'react';
 
 export default function Home({ allProducts }) {
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    setFilteredProducts(
+      allProducts.sort(function (a, b) {
+        return (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)
+      }))
+  }, [])
+
   return (
     <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 bg-gray-100 p-8">
 
@@ -11,11 +21,11 @@ export default function Home({ allProducts }) {
         <Image src={logo} quality={100} width="94.5px" height="36.875px" />
       </div>
       <div className="flex justify-between items-center col-span-2 md:col-span-3 lg:col-span-4 xl:col-span-5 px-2">
-        <h1 className="font-bold text-2xl uppercase">FUTEBOL</h1>
-        <span className="text-xs">{allProducts.length} RESULTADOS ENCONTRADOS</span>
+        <h1 className="font-bold text-2xl uppercase">catálogo</h1>
+        <span className="text-xs">{filteredProducts.length} RESULTADOS ENCONTRADOS</span>
       </div>
 
-      {allProducts.map((product, index) => (
+      {filteredProducts.map((product, index) => (
         <div key={product.id} className="flex items-center flex-col bg-gray-50 shadow-lg px-2 py-4 rounded-md">
           <img src={product.media.source} className="w-56" />
           <div className="flex flex-col items-center justify-center flex-wrap mt-6">
